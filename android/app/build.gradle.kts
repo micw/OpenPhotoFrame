17a2cbc77152578abb29556b2e792b6df13fc94f
Brad Pitcher <bradpitcher@gmail.com>
2026-06-04 22:32:05 -0700

    Merge pull request #2 from brad/feature-android-screen-saver-support-13193880336557798301
    Enable Android "Screen saver" (DreamService) support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "io.github.micw.openphotoframe"
-    compileSdk = 37
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     // Disable dependency metadata in APK signature block (required for F-Droid reproducible builds)
@@ -41,7 +41,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
---
+++ android/app/src/main/kotlin/io/github/micw/openphotoframe/PhotoFrameDreamService.kt
@@ -0,0 +1,19 @@
+package io.github.micw.openphotoframe
+
+import android.content.Intent
+import android.service.dreams.DreamService
+
+class PhotoFrameDreamService : DreamService() {
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        isInteractive = false
+        isFullscreen = true
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+        finish()
+    }
+}
